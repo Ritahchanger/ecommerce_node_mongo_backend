d94d5bccaddc0ab1db5c4774670fa540c9f41a3e
Dennis Peter <peterdennis573@gmail.com>
2024-02-22 15:45:57 +0300

    -Updated files and set up login api

--- routes/Product.js
+++ routes/Product.js
@@ -2,13 +2,23 @@ const Product = require("../models/products.model");
 const express = require("express");
 const router = express.Router();
 
-
 router.get("/get_products", async (req, res) => {
-
-  const products=await Product.find({});
-  res.status(200).send(products);
+    try {
+        const { filter, value } = req.query;
+        const products = await Product.find({});
+        if (!filter || !value) {
+            return res.status(200).send(products);
+        } else {
+            const filteredProducts = products.filter(product => {
+                return product[filter] && product[filter].toLowerCase().includes(value.toLowerCase());
+            });
+            return res.status(200).send(filteredProducts);
+        }
+    } catch (error) {
+        return res.status(500).send({ msg: `${error.message}` });
+    }
 });
-
+ 
 router.post("/post_products", async (req, res) => {
   try {
     const newProduct = await Product.create(req.body);
@@ -17,5 +27,50 @@ router.post("/post_products", async (req, res) => {
     return res.status(400).send({ msg: `${error.message}` });
   }
 });
+
+
+router.put("/update_products/:product_name",async (req,res) =>{
+    try{
+        const { product_name } = req.params;
+        const updatedProducts = req.body;
+
+        const product = await Product.findOneAndUpdate(
+            {name:product_name},
+            updatedProducts,
+            {new:true}
+        )
+
+        if(!product){
+            return res.status(404).send({msg:"Product not found"})
+        }
+        
+        return res.status(200).send(product);
+    }catch(error){
+        res.status(500).send({msg:`${error.message}`});
+    }
+
+} )
+
+
+router.delete("/delete_products/:id",async(req,res)=>{
+    const { id } = req.params;
+
+    try{
+        const deleteProduct = await Product.findByIdAndDelete(id);
+
+        if(!deleteProduct){
+            return res.status(404).send({msg:"Product not found"});
+        }else{
+            return res.status(200).send({
+                msg:"Product deleted successfully"
+            })
+        }
+
+    }catch(error){
+        return res.status(404).json({msg:`${error.message}`});
+    }
+
+})
+
 
 module.exports = router;
--- routes/User.js
+++ routes/User.js
@@ -4,95 +4,106 @@ const router = express.Router();
 
 router.get("/get_users", async (req, res) => {
   try {
-
-    const {filter,value}=req.query;
-    const users=await User.find({});
-    if(!filter || !value){
-        return res.status(200).send(users);
-    }else{
-        const filteredUsers=users.filter(user=>{
-            return user[filter] && user[filter].toLowerCase().includes(value.toLowerCase());
-        });
-        return res.status(200).send(filteredUsers);
+    const { filter, value } = req.query;
+    const users = await User.find({});
+    if (!filter || !value) {
+      return res.status(200).send(users);
+    } else {
+      const filteredUsers = users.filter((user) => {
+        return (
+          user[filter] &&
+          user[filter].toLowerCase().includes(value.toLowerCase())
+        );
+      });
+      return res.status(200).send(filteredUsers);
     }
-
   } catch (error) {
     res.status(400).json({ msg: `${error.message}` });
   }
 });
 
-
 router.post("/post_users", async (req, res) => {
-    try {
-      const { firstName, lastname, username, email, idNo, password } = req.body;
-      const emailFound = await User.findOne({ email });
-      const usernameFound = await User.findOne({ username });
-      const idNoFound = await User.findOne({ idNo });
-      if (emailFound || usernameFound || idNoFound) {
-        return res.status(400).json({
-          msg: "User not created",
-          emailFound: !!emailFound,
-          usernameFound: !!usernameFound,
-          idNoFound: !!idNoFound
-        });
-      } else {
-        const newUser=await User.create(req.body);
-        return res.status(200).json({
-            msg:"The user is successfully registered"
-        })
+  try {
+    const { firstName, lastname, username, email, idNo, password } = req.body;
+    const emailFound = await User.findOne({ email });
+    const usernameFound = await User.findOne({ username });
+    const idNoFound = await User.findOne({ idNo });
+    if (emailFound || usernameFound || idNoFound) {
+      return res.status(400).json({
+        msg: "User not created",
+        emailFound: !!emailFound,
+        usernameFound: !!usernameFound,
+        idNoFound: !!idNoFound,
+      });
+    } else {
+      const newUser = await User.create(req.body);
+      return res.status(200).json({
+        msg: "The user is successfully registered",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({ msg: "Something went wrong with your server" });
+  }
+});
+
+router.delete("/delete_users/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deleteUser = await User.findByIdAndDelete(id);
+    if (!deleteUser) {
+      return res.status(404).json({ msg: "User not found" });
+    } else {
+      return res.status(200).json({
+        msg: "User deleted successfully",
+      });
+    }
+  } catch (error) {
+    res.status(404).json({ msg: `${error.message}` });
+  }
+});
+
+router.put("/update_user/:user_name", async (req, res) => {
+  try {
+    const { user_name } = req.params;
+    const { firstName, lastName, email, idNo, password } = req.body;
+    const allowedUpdates = { firstName, lastName, email, idNo, password };
+    const updates = Object.keys(allowedUpdates).reduce((obj, key) => {
+      if (allowedUpdates[key] !== undefined) {
+        obj[key] = allowedUpdates[key];
       }
-    } catch (error) {
-      res.status(500).json({ msg: "Something went wrong with your server" });
+      return obj;
+    }, {});
+
+    const user = await User.findOne({ username: user_name });
+    if (!user) {
+      return res.status(404).json({ msg: "User not found" });
     }
-  });
+    const updatedUser = await User.findOneAndUpdate(
+      { username: user_name },
+      updates,
+      { new: true }
+    );
 
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: "Internal Server Error" });
+  }
+});
 
-  router.delete("/delete_users/:id",async (req,res)=>{
-    const { id } = req.params;
-    try{
-        const deleteUser=await User.findByIdAndDelete(id);
-        if(!deleteUser){
-            return res.status(404).json({msg:"User not found"});
-        }else{
-            return res.status(200).json({
-                msg:"User deleted successfully"
-            })
-        }
-    }catch(error){
-        res.status(404).json({msg:`${error.message}`});
+router.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username,password });
+
+    if(!user){
+      return res.status(400).json({msg:'Invalid credentials'})
     }
-  })
+    res.status(200).json({msg:"User is validated"});
 
-  router.put("/update_user/:user_name", async (req, res) => {
-    try {
-      const { user_name } = req.params;
-      const { firstName, lastName, email, idNo, password } = req.body;
-      const allowedUpdates = { firstName, lastName, email, idNo, password };
-      const updates = Object.keys(allowedUpdates).reduce((obj, key) => {
-        if (allowedUpdates[key] !== undefined) {
-          obj[key] = allowedUpdates[key];
-        }
-        return obj;
-      }, {});
-
-      const user = await User.findOne({ username: user_name });
-      if (!user) {
-        return res.status(404).json({ msg: "User not found" });
-      }
-      const updatedUser = await User.findOneAndUpdate(
-        { username: user_name },
-        updates,
-        { new: true }
-      );
-  
-      res.status(200).json(updatedUser);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ msg: "Internal Server Error" });
-    }
-  });
-  
-  
-
+  } catch (error) {
+    res.status(500).send({ msg: `${error.message}` });
+  }
+});
 
 module.exports = router;
